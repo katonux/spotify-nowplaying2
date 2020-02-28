5406c27bc1be10b5f4a4079bd937648fb7a859a4
Shota Kato <sheeptech@vivaldi.net>
2020-02-28 23:49:05 +0900

    router設定

--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,16 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+import NowPlaying from '@/views/NowPlaying.vue'
 
 Vue.use(VueRouter)
 
-const routes = []
+const routes = [
+  {
+    path: '/',
+    name: 'NowPlaying',
+    component: NowPlaying
+  }
+]
 
 const router = new VueRouter({
   mode: 'history',
